aed4ae48f6bb2ac89215f65b01e58199b40f84d6
carrotRakko <24588751+carrotRakko@users.noreply.github.com>
2021-04-09 23:13:16 +0900

    Update combination.php

--- combination.php
+++ combination.php
@@ -19,10 +19,30 @@ function combination(int $n, int $k): array
 }
 
 // 10進表記なので合ってるのかわかりにくい
-var_dump(combination(16, 4));
+// var_dump(combination(16, 4));
 
 // 2進表記にするとちょっとわかりやすくなる
-var_dump(array_map('decbin', combination(16, 4)));
+// var_dump(array_map('decbin', combination(16, 4)));
 
 // 0埋めするともうちょっとわかりやすくなる
-var_dump(array_map(fn ($c) => str_pad(decbin($c), 16, '0', STR_PAD_LEFT), combination(16, 4)));
+// var_dump(array_map(fn ($c) => str_pad(decbin($c), 16, '0', STR_PAD_LEFT), combination(16, 4)));
+
+function visualize(int $placement): void
+{
+    for ($col = 0; $col <= 3; $col++) {
+        for ($row = 0; $row <= 3; $row++) {
+            if (($placement >> (15 - 4 * $col - $row)) % 2 === 1) {
+                echo '#';
+            } else {
+                echo '.';
+            }
+        }
+        echo "\n";
+    }
+    echo "\n";
+}
+
+// 表示してみよう
+foreach (combination(16, 4) as $placement) {
+    visualize($placement);
+}
